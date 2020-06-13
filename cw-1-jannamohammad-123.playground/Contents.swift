39071917e4c7c3b698fa35028023f0b22ded92b9
jannalari <66527740+jannalari@users.noreply.github.com>
2020-06-13 21:48:25 +0300

    done

---
+++ cw-1-jannamohammad-123.playground/Contents.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+var str = "Hello, playground"
+let myname = "janna"
+var hobby1 = "swimming"
+var hobby2 = "playing piano"
+var age = 14
+var height = 1.63
+print ( "my nanme is \(myname) my age is \(age) and my height is \(height) my first hobby is \(hobby1) and my second hobby is \(hobby2)")
+
+//
+var friendhobby1 = "running"
+var friendhobby2 = "walking"
+var friendname = "alyaa"
+var friendheight = 1.64
+var friendage = 13
+//
+hobby1 == friendhobby1
+hobby1 != friendhobby1
+hobby2 == friendhobby2
+hobby2 != friendhobby2
+
+age > friendage
+age < friendage
+age >= friendage
+age <= friendage
+age == friendage
+age != friendage
+
+height == friendheight
+height < friendheight
+height > friendheight
+height >= friendheight
+height <= friendheight
+height != friendheight
+
+
+
+
